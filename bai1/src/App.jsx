7acdf808e968bbbf005c1515dcda6faf45426a7b
Tran Hieu <23710931.hieu@student.iuh.edu.vn>
2026-04-07 19:39:42 +0700

    first commit

---
+++ bai1/src/App.jsx
@@ -0,0 +1,27 @@
+import Home from "./components/Home"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+      <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
